63d94f26f80d80d192585b09199e24f073e046cf
Cinti <Cinti@Cinti-PC>
2014-11-19 13:53:03 +0100

    Prog4_1

--- Ejercicio2/src/Prog1_0.java
+++ Ejercicio2/src/Prog1_0.java
@@ -6,9 +6,21 @@ public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	Scanner leerradio = new Scanner(System.in); 
 	int radio; 
+	double volumen; 
+	double area;
+	double longitud;
 	System.out.println("Introduce el radio de la esfera"); 
-	radio=leerradio.nextInt(); 
+	radio=leerradio.nextInt();
+	volumen = (4 * (Math.PI) * (Math.pow(radio,3))/3);
+	System.out.println("El volumen de la esfera cuyo radio "+radio+" es " + volumen); 
+	System.out.println("Finalizado Version 1");
+	area = (Math.PI) * (Math.pow(radio, 2)); 
+	System.out.println("El área de la esfera cuyo radio "+radio+" es " + area);
+	System.out.println("Finalizado Version 2");
+	longitud = 2 * (Math.PI)* radio;
+	System.out.println("La longitud de la circunferencia cuyo radio "+radio+" es "+longitud); 
+	System.out.println("Finalizado Version 3");
 	System.out.println(""); 
-	 System.out.println("Version definitiva y refundida");
+	System.out.println("Version definitiva y refundida");
  } 
 } 
