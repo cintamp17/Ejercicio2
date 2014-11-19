c916915c0ee14fbe7f0457ddaeb86a1c19d48850
Cinti <Cinti@Cinti-PC>
2014-11-19 13:14:15 +0100

    Prog3_0

--- Ejercicio2/src/Prog1_0.java
+++ Ejercicio2/src/Prog1_0.java
@@ -5,8 +5,10 @@ public class Prog1_0 {
 public static void main(String[] args) { 
 	// TODO Auto-generated method stub
 	Scanner leerradio = new Scanner(System.in); 
-	int radio; 
+	int radio;
+	double longitud;
 	System.out.println("Introduce el radio de la esfera"); 
 	radio=leerradio.nextInt(); 
+	longitud = 2 * (Math.PI)* radio;
  } 
 } 
