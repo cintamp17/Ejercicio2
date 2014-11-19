96f837254c4d05ad64cf8e069105cfbbb27b8c43
Cinti <Cinti@Cinti-PC>
2014-11-19 13:12:39 +0100

    Prog2_0

--- Ejercicio2/src/Prog1_0.java
+++ Ejercicio2/src/Prog1_0.java
@@ -6,7 +6,11 @@ public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	Scanner leerradio = new Scanner(System.in); 
 	int radio; 
+	double area;
 	System.out.println("Introduce el radio de la esfera"); 
 	radio=leerradio.nextInt(); 
+	area = (Math.PI) * (Math.pow(radio, 2)); 
+	 System.out.println("El área de la esfera cuyo radio "+radio+" es " + area);
+	System.out.println("Finalizado Version 2");
  } 
 } 
