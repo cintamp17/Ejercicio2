857f3c1a234d9f6e1f8c424faf99335217424485
Cinti <Cinti@Cinti-PC>
2014-11-19 13:09:32 +0100

    Prog1_2

--- Ejercicio2/src/Prog1_0.java
+++ Ejercicio2/src/Prog1_0.java
@@ -1,12 +1,15 @@
 import java.util.Scanner; 
 import java.lang.Math; 
 public class Prog1_0 { 
-double volumen;
+
 public static void main(String[] args) { 
 	// TODO Auto-generated method stub
 	Scanner leerradio = new Scanner(System.in); 
 	int radio; 
+	double volumen;
 	System.out.println("Introduce el radio de la esfera"); 
 	radio=leerradio.nextInt(); 
+	volumen = (4 * (Math.PI) * (Math.pow(radio,3))/3);
  } 
+
 } 
