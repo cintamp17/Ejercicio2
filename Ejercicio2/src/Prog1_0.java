3b8e3d2ebf6a26c621e22c8afed6aa08f85a0393
Cinti <Cinti@Cinti-PC>
2014-11-19 13:07:13 +0100

    Prog1_1

--- Ejercicio2/src/Prog1_0.java
+++ Ejercicio2/src/Prog1_0.java
@@ -1,7 +1,7 @@
 import java.util.Scanner; 
 import java.lang.Math; 
 public class Prog1_0 { 
- 
+double volumen;
 public static void main(String[] args) { 
 	// TODO Auto-generated method stub
 	Scanner leerradio = new Scanner(System.in); 
